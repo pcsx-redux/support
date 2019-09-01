0edb111e38491fc77a2c5722b918535c91d99033
Nicolas 'Pixel' Noble <nicolas@nobis-crew.org>
2019-08-31 22:54:45 -0700

    We can't trust clang's stdint.h.

--- src/mips/common/compiler/stdint.h
+++ src/mips/common/compiler/stdint.h
@@ -19,7 +19,9 @@
 
 #pragma once
 
-#include "common/compiler/stdint.h"
-
-uint32_t readCOP0Status();
-void writeCOP0Status(uint32_t);
+typedef signed char int8_t;
+typedef signed short int16_t;
+typedef signed int int32_t;
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
