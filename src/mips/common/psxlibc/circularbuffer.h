8cea766e6d1db47a5c9b72266200a379f5e71c72
Nicolas Pixel Noble <pixel@nobis-crew.org>
2020-04-16 23:29:32 -0700

    Few more syscalls implemented.

--- src/mips/common/psxlibc/circularbuffer.h
+++ src/mips/common/psxlibc/circularbuffer.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2019 PCSX-Redux authors                                 *
+ *   Copyright (C) 2020 PCSX-Redux authors                                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
@@ -21,36 +21,14 @@
 
 #include "common/compiler/stdint.h"
 
-#define HW_U8(x) (*(volatile uint8_t *)(x))
-#define HW_U16(x) (*(volatile uint16_t *)(x))
-#define HW_U32(x) (*(volatile uint32_t *)(x))
-#define HW_S8(x) (*(volatile int8_t *)(x))
-#define HW_S16(x) (*(volatile int16_t *)(x))
-#define HW_S32(x) (*(volatile int32_t *)(x))
+enum {
+    PSXCIRC_RAW = 1,
+    PSXCIRC_STOPPED = 2,
+    PSXCIRC_BREAK = 4,
+};
 
-#define SPU_MVOL_L HW_U16(0x1f801d80)
-#define SPU_MVOL_R HW_U16(0x1f801d82)
-#define SPU_REVERB_L HW_U16(0x1f801d84)
-#define SPU_REVERB_R HW_U16(0x1f801d86)
-
-#define SIO1_DATA HW_U8(0x1f801050)
-#define SIO1_STAT HW_U16(0x1f801054)
-#define SIO1_MODE HW_U16(0x1f801058)
-#define SIO1_CTRL HW_U16(0x1f80105a)
-#define SIO1_BAUD HW_U16(0x1f80105e)
-
-#define POST HW_U8(0xbf802041)
-
-#define IREG HW_U32(0x1f801070)
-#define IMASK HW_U32(0x1f801074)
-
-#define DPCR HW_U32(0x1f8010f0)
-#define DICR HW_U32(0x1f8010f4)
-
-#define GPU_DATA HW_U32(0x1f801810)
-#define GPU_STATUS HW_U32(0x1f801814)
-
-#define ATCONS_STAT HW_U8(0x1f802000)
-#define ATCONS_FIFO HW_U8(0x1f802002)
-#define ATCONS_IRQ  HW_U8(0x1f802030)
-#define ATCONS_IRQ2 HW_U8(0x1f802032)
+struct CircularBuffer {
+    uint32_t flags;
+    uint8_t * start, * end;
+    uint8_t buffer[256];
+};
